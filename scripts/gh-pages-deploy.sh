18e71d82625218a462d4e879f1f0564df2b259bf
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-08-14 12:53:12 -0500

    Fix branch switch.

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -2,11 +2,12 @@
 # exit on error
 set -o errexit
 
+current_branch=$(git rev-parse --abbrev-ref HEAD)
 git checkout --orphan gh-pages
 pnpm build
 git --work-tree dist add --all
 git --work-tree dist commit -m 'Deploy'
 git push origin HEAD:gh-pages --force
 rm -r dist
-git checkout -f main
+git checkout -f "$current_branch"
 git branch -D gh-pages
